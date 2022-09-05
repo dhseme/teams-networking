d708b5b1a8a771bdbe7e4343717fbdf9e0e5fc8c
Mihai Semenescu <mihai.dh.semenescu@gmail.com>
2022-09-05 19:19:55 +0300

    commit

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-function getTeamHTLM(team) {
-  return `<tr>
-      
- <td>${team.promotion}</td>
- <td>${team.members}</td>
- <td>${team.name}</td>
- <td><a href="${team.url}">open</a>
- </td>
- <td>x e</td>
-</tr>`;
+function $(selector) {
+  return document.querySelector(selector);
 }
 
+function getTeamHTML(team) {
+  return `
+  <tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>
+      <a href="${team.url}">open</a>
+    </td>
+    <td>x e</td>
+  </tr>`;
+}
 function displayTeams(teams) {
-  // var teamsHTML = "";
-  // teams.forEach(function (team) {
-  //   teamsHTML += getTeamHTLM(team);
-  // });
-
-  var teamsHTML = teams.map(getTeamHTLM);
-
-  document.querySelector("table tbody").innerHTML = teamsHTML.join(";");
+  const teamsHTML = teams.map(getTeamHTML);
+  // afisare
+  $("table tbody").innerHTML = teamsHTML.join("");
 }
-
 function loadTeams() {
   fetch("data/teams.json")
-    .then(function (r) {
-      return r.json();
-    })
-    .then(function (teams) {
+    .then((r) => r.json())
+    .then((teams) => {
       displayTeams(teams);
     });
 }
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  const promotion = $("input[name=promotion]").value;
+  const members = $("input[name=members]").value;
+  const name = $("input[name=name]").value;
+  const url = $("input[name=url]").value;
+
+  const team = {
+    promotion: promotion,
+    members: members,
+    name: name,
+    url: url,
+  };
+  console.warn("adauga in teams.json:", JSON.stringify(team));
+}
+
+function initEvents() {
+  const form = document.getElementById("editForm");
+  console.warn("form", form);
+  form.addEventListener("submit", submitForm);
+}
+
 loadTeams();
+initEvents();
