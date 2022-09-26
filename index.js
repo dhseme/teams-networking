d0e88fe19c3418b758a44ebe504a6882fb21b509
Mihai Semenescu <mihai.dh.semenescu@gmail.com>
2022-09-26 18:31:39 +0300

    A commit

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function createTeamRequest(team) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(team),
-  });
+  }).then((r) => r.json());
 }
 
 function removeTeamRequest(id) {
@@ -55,6 +55,16 @@ function removeTeamRequest(id) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ id: id }),
+  }).then((r) => r.json());
+}
+
+function updateTeamRequest(team) {
+  fetch("http://localhost:3000/teams-json/update", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(team),
   }).then((r) => r.json());
 }
 
@@ -86,29 +96,36 @@ function submitForm(e) {
   const team = getFormValues();
 
   if (editId) {
-    console.warn("pls edit id", editId, team);
-  } else
-    createTeamRequest(team)
-      .then((r) => r.json())
-      .then((status) => {
-        console.warn("status", status);
-        if (status.success) {
-          location.reload();
-        }
-      });
+    team.id = editId;
+    updateTeamRequest(team).then((status) => {
+      if (status.success) {
+        $("#editForm").reset();
+        loadTeams();
+      }
+    });
+  } else {
+    createTeamRequest(team).then((status) => {
+      if (status.success) {
+        $("#editForm").reset();
+        loadTeams();
+      }
+    });
+  }
 }
 
 function startEditTeam(id) {
   const team = allTeams.find((team) => team.id === id);
-  console.warn("edit", id, team);
   setFormValues(team);
   editId = id;
 }
 
 function initEvents() {
-  const form = document.getElementById("editForm");
+  const form = $("#editForm");
   form.addEventListener("submit", submitForm);
-
+  form.addEventListener("reset", () => {
+    console.warn("reset");
+    editId = undefined;
+  });
   form.querySelector("tbody").addEventListener("click", (e) => {
     if (e.target.matches("a.delete-btn")) {
       const id = e.target.getAttribute("data-id");
